1a934b2e219edeadf5e3d25eac015874bb38b9db
YannickSchall <yannickschall@gmx.de>
2023-04-26 01:27:20 +0200

    Refactor GameBoard to fix JSON

--- model/src/main/scala/gameboard/gameBoardBaseImpl/GameBoard.scala
+++ model/src/main/scala/gameboard/gameBoardBaseImpl/GameBoard.scala
@@ -16,21 +16,21 @@ GameBoard @Inject() (fields: Matrix[Field]) extends GameBoardInterface {
 
   val size: Int = fields.size
 
-  def getField(pos: String): Field = {
+  override def getField(pos: String): Field = {
     field(Integer.parseInt(pos.tail)-1, pos.charAt(0).toInt - 65)
   }
 
-  def remove(row: Int, col: Int): GameBoard = copy(fields.replaceField(row, col, Field(posToStr(row, col), None)))
+  override def remove(row: Int, col: Int): GameBoard = copy(fields.replaceField(row, col, Field(posToStr(row, col), None)))
 
-  def set(row: Int, col: Int, piece: Option[Piece]): GameBoard = copy(fields.replaceField(row, col, Field(posToStr(row, col), piece)))
+  override def set(row: Int, col: Int, piece: Option[Piece]): GameBoard = copy(fields.replaceField(row, col, Field(posToStr(row, col), piece)))
 
-  def field(row: Int, col: Int): Field = fields.field(row, col)
+  override def field(row: Int, col: Int): Field = fields.field(row, col)
 
-  def colToInt(pos: String): Int = pos.charAt(0).toInt - 65
+  override def colToInt(pos: String): Int = pos.charAt(0).toInt - 65
 
-  def rowToInt(pos: String): Int = Integer.parseInt(pos.tail) - 1
+  override def rowToInt(pos: String): Int = Integer.parseInt(pos.tail) - 1
 
-  def posToStr(row: Int, col: Int): String = (col + 65).toChar.toString + (row+1).toString
+  override def posToStr(row: Int, col: Int): String = (col + 65).toChar.toString + (row+1).toString
 
 
   override def toString: String = {
@@ -45,48 +45,39 @@ GameBoard @Inject() (fields: Matrix[Field]) extends GameBoardInterface {
   }
 
 
-  def getPiece(row: Int, col: Int): Option[Piece] = {
+  override def getPiece(row: Int, col: Int): Option[Piece] = {
     def pos = posToStr(row, col)
     getField(pos).piece
   }
 
-  def move(start: String, dest: String): GameBoard = {
+  override def move(start: String, dest: String): GameBoard = {
     getField(start).piece match {
       case Some(piece) => remove(Integer.parseInt(start.tail)-1, start.charAt(0).toInt - 65).set(Integer.parseInt(dest.tail)-1, dest.charAt(0).toInt - 65, Some(Piece(piece.state, Integer.parseInt(dest.tail)-1, dest.charAt(0).toInt - 65, piece.getColor)))
       case None => print("Field " + start + " is empty"); this
     }
   }
 
-  def movePossible(start: String, dest: String): Mover = {
+  override def movePossible(start: String, dest: String): Mover = {
     getField(start).piece match {
       case Some(piece) => piece.movePossible(dest, this)
       case _ => new Mover(false, "", false)
     }
   }
 
-  implicit def optionFormat[T: Format]: Format[Option[T]] = new Format[Option[T]] {
-    override def reads(json: JsValue): JsResult[Option[T]] = json.validateOpt[T]
 
-    override def writes(o: Option[T]): JsValue = o match {
-      case Some(t) ⇒ implicitly[Writes[T]].writes(t)
-      case None ⇒ JsNull
-    }
-  }
-
-
-  implicit val pieceReads: Reads[Piece] = (
+  override implicit val pieceReads: Reads[Piece] = (
     (JsPath \ "state").read[String] and
       (JsPath \ "prow").read[Int] and
       (JsPath \ "pcol").read[Int] and
       (JsPath \ "color").read[Color]
     )(Piece.apply _)
 
-  implicit val fieldReads: Reads[Field] = (
+  override implicit val fieldReads: Reads[Field] = (
     (JsPath \ "pos").read[String] and
       (JsPath \ "piece").readNullable[Piece]
     )(Field.apply _)
 
-  implicit val colorReads: Reads[Color] = Reads { json =>
+  override implicit val colorReads: Reads[Color] = Reads { json =>
     json.validate[String].flatMap {
       case "white" => JsSuccess(Color.White)
       case "black" => JsSuccess(Color.Black)
@@ -94,31 +85,38 @@ GameBoard @Inject() (fields: Matrix[Field]) extends GameBoardInterface {
     }
   }
 
-  implicit val colorWrites: Writes[Color] = new Writes[Color] {
-    def writes(color: Color) = JsString(color.color)
+  override implicit val colorWrites: Writes[Color] = new Writes[Color] {
+    def writes(color: Color) = Json.obj(
+      "color" -> color.color,
+    )
   }
 
-  implicit val fieldWrites: Writes[Field] = new Writes[Field] {
+  override implicit val fieldWrites: Writes[Field] = new Writes[Field] {
     def writes(field: Field) = Json.obj(
       "pos" -> field.getPos,
-      "piece" -> pieceWrites.writes(field.getPiece.get)
+      "piece" -> pieceWrites.writes(field.getPiece)
     )
   }
 
-  implicit val pieceWrites: Writes[Piece] = new Writes[Piece] {
-    def writes(piece: Piece) = Json.obj(
-      "state" -> piece.state,
-      "prow" -> piece.row,
-      "pcol" -> piece.col,
-      "color" -> colorWrites.writes(piece.getColor)
-    )
+  override implicit val pieceWrites: Writes[Option[Piece]] = new Writes[Option[Piece]] {
+    def writes(piece: Option[Piece]) = {
+      if (piece.isDefined) {
+        Json.obj(
+          "state" -> piece.get.state,
+          "prow" -> piece.get.row,
+          "pcol" -> piece.get.col,
+          "color" -> colorWrites.writes(piece.get.getColor)
+        )
+      } else {
+        JsNull
+      }
+    }
   }
-
 
   def gameBoardToJson = {
     Json.obj(
       "gameBoard" -> Json.obj(
-        "size" -> JsNumber(this.size),
+        "size" -> this.size,
         "fields" -> Json.toJson(
           for {
             row <- 0 until this.size
@@ -127,7 +125,7 @@ GameBoard @Inject() (fields: Matrix[Field]) extends GameBoardInterface {
             Json.obj(
               "row" -> row,
               "col" -> col,
-              "field" -> Json.toJson(this.field(row, col))
+              "field" -> field(row, col)
             )
           }
         )
@@ -135,13 +133,15 @@ GameBoard @Inject() (fields: Matrix[Field]) extends GameBoardInterface {
     )
   }
 
-  def jsonToString: String =
+
+
+  override def jsonToString: String =
     Json.prettyPrint(gameBoardToJson)
 
-  def toJson: JsValue =
+  override def toJson: JsValue =
     gameBoardToJson
 
-  override def jsonToGameBoard(gameBoard: GameBoardInterface): GameBoardInterface = {
+  override def jsonToGameBoard(): GameBoardInterface = {
     var gameBoard: GameBoardInterface = null
     val source: String = Source.fromFile("gameBoard.json").getLines.mkString
     val json: JsValue = Json.parse(source)
