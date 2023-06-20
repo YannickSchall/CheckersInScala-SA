cc615b3a0d3b3107b5850775c107de18e2f28f32
YannickSchall <yannickschall@gmx.de>
2023-06-20 19:42:34 +0200

    Added tests

---
+++ src/test/scala/aview/TuiSpec.scala
@@ -0,0 +1,27 @@
+package aview
+import aview.Tui
+import controller.controllerComponent.controllerBaseImpl.Controller
+import model.gameBoardBaseImpl.{GameBoard, GameBoardCreator}
+import org.scalatest.*
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.*
+
+class TuiSpec extends  AnyWordSpec {
+  "A tui" should {
+    val controller = new Controller(new GameBoard(8))
+    val tui = new Tui(controller)
+    val testGB8 = new GameBoardCreator(8).createBoard()
+    val testGB10 = new GameBoardCreator(10).createBoard()
+    "create us a 8x8 gameboard when typing 'new8' " in {
+      tui.tuiEntry("new 8")
+      controller.gameBoard should be (testGB8)
+    }
+    "create us a 10x10 gameboard when typing 'new10' " in {
+      tui.tuiEntry("new 10")
+      controller.gameBoard should be (testGB10)
+    }
+    "have a size" in {
+      tui.size should be (10)
+    }
+  }
+}
